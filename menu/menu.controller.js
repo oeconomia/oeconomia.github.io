4dc1e8df7db04dad643bce664881db31b246ee95
Alexei Fedotov <alexei.fedotov@gmail.com>
2017-07-14 19:06:56 +0300

    intermediate

---
+++ menu/menu.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict'
+
+    angular
+        .module('app')
+        .controller('MenuController', MenuController)
+
+    MenuController.$inject = ['UserService', '$rootScope']
+    function MenuController(UserService, $rootScope) {
+        function initController() {
+        }
+
+        function login() {
+        }
+        
+        function logout() {
+        }
+    }
+
+})()

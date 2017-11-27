e7741e56a129179fd1c49fa3549fe156df7297b8
Alexei Fedotov <alexei.fedotov@gmail.com>
2017-11-27 09:40:33 +0300

    fixing bugs

--- clan/clan.controller.js
+++ clan/clan.controller.js
@@ -3,43 +3,11 @@
 
     angular
         .module('app')
-        .controller('HomeController', HomeController)
+        .controller('ClanController', ClanController)
 
-    HomeController.$inject = ['UserService', '$rootScope']
-    function HomeController(UserService, $rootScope) {
-        var vm = this
+    ClanController.$inject = ['UserService', '$rootScope']
+    function ClanController(UserService, $rootScope) {
 
-        vm.user = null
-        vm.allUsers = []
-        vm.deleteUser = deleteUser
-
-        initController()
-
-        function initController() {
-            loadCurrentUser()
-            loadAllUsers()
-        }
-
-        function loadCurrentUser() {
-            UserService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
-                    vm.user = user
-                })
-        }
-
-        function loadAllUsers() {
-            UserService.GetAll()
-                .then(function (users) {
-                    vm.allUsers = users;
-                })
-        }
-
-        function deleteUser(id) {
-            UserService.Delete(id)
-            .then(function () {
-                loadAllUsers();
-            })
-        }
     }
 
 })()
